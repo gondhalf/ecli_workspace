fa7641e23fe9d5f78630fb727f0090be8ccbe9a1
gondhalf <pkt_coma18@hotmail.com>
2017-03-26 22:52:17 +0200

    COn juego3d

--- juegoTRESd/src/engineTester/MainGameLoop.java
+++ juegoTRESd/src/engineTester/MainGameLoop.java
@@ -1,12 +1,17 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+import entities.Entity;
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -14,23 +19,51 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 
-		float[] vertices = { -0.5f, 0.5f, 0, // V0
-				-0.5f, -0.5f, 0, // V1
-				0.5f, -0.5f, 0, // V2
-				0.5f, 0.5f, 0 }; // V3
+		// ESTO ES UN CUBE
 
-		int[] indices = { 0, 1, 3, // Top left triangle (V0,V1,V3)
-				3, 1, 2 }; // Bottom right triangle (V3,V1,V2)
+		float[] vertices = { -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
 
-		RawModel model = loader.loadToVAO(vertices, indices);
+				-0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+
+				0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+
+				-0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
+
+				-0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f,
+
+				-0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f
+
+		};
+
+		float[] textureCoords = {
+
+				0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+				1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0
+
+		};
+
+		int[] indices = { 0, 1, 3, 3, 1, 2, 4, 5, 7, 7, 5, 6, 8, 9, 11, 11, 9, 10, 12, 13, 15, 15, 13, 14, 16, 17, 19,
+				19, 17, 18, 20, 21, 23, 23, 21, 22
+
+		};
+		// FIN DEL CUBE
+
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("textura1"));
+		TexturedModel staticModel = new TexturedModel(model, texture);
+		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		Camera camera = new Camera();
 
 		while (!Display.isCloseRequested()) {
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.prepare();
 			shader.start();
-			renderer.render(model);
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
